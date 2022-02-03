c9ec59b05535aa4cd464b3550a71158bb82b5e09
ricardo-afonso <ricardoafonso.gd@gmail.com>
2022-02-03 15:41:59 +0000

    Add template file

---
+++ challenges/template.js
@@ -0,0 +1,30 @@
+/*
+
+Challenge Description
+
+*/
+
+const { expect } = require("@jest/globals");
+const { describe } = require("yargs");
+
+
+//Solution
+
+let fn = () => {}
+
+
+//Testing
+describe('Challenge 000', () => {
+  it('runs', () => {
+    expect().toBe()
+  })
+})
+
+
+
+//Notes
+/*
+
+Explanation
+
+*/
